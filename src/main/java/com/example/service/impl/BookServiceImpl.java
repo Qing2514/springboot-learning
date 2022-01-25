148cc17b024ef9db48cb2efbd2d75f8aed74db3e
Qing2514 <2514632453@qq.com>
2022-01-25 20:49:17 +0800

    feat(): 添加前端分页功能

--- src/main/java/com/example/service/impl/BookServiceImpl.java
+++ src/main/java/com/example/service/impl/BookServiceImpl.java
@@ -25,6 +25,16 @@ public class BookServiceImpl extends ServiceImpl<BookDao, Book> implements IBook
     public IPage<Book> getPage(int currentPage, int pageSize) {
         IPage page = new Page(currentPage, pageSize);
         bookDao.selectPage(page, null);
+        // // 当前页数
+        // System.out.println(page.getCurrent());
+        // // 每页数据数
+        // System.out.println(page.getSize());
+        // // 数据总数
+        // System.out.println(page.getTotal());
+        // // 总共页数
+        // System.out.println(page.getPages());
+        // // 具体所有数据
+        // System.out.println(page.getRecords());
         return page;
     }
 }
