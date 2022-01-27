9e82f20ad9f9a572cd5abd12145d770bbad95e98
Qing2514 <2514632453@qq.com>
2022-01-27 15:37:56 +0800

    feat: main 函数断开读取外部临时配置对应的入口，即不接受外部参数

--- src/main/java/com/example/SpringbootApplication.java
+++ src/main/java/com/example/SpringbootApplication.java
@@ -8,6 +8,9 @@ public class SpringbootApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringbootApplication.class, args);
+
+        // 断开读取外部临时配置对应的入口，也就是去掉读取外部参数的形参
+        // SpringApplication.run(SpringbootApplication.class);
     }
 
 }
