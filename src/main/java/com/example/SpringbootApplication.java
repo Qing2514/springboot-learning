732097cf1ef2961f2bbe02bfb0a99fe540713443
Qing2514 <2514632453@qq.com>
2022-01-30 17:10:14 +0800

    feat: 发送虚拟请求

--- src/main/java/com/example/SpringbootApplication.java
+++ src/main/java/com/example/SpringbootApplication.java
@@ -1,18 +1,13 @@
 package com.example;
 
-import com.alibaba.druid.pool.DruidDataSource;
-import com.example.domain.Source;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.context.ConfigurableApplicationContext;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class SpringbootApplication {
 
     public static void main(String[] args) {
-        // SpringApplication.run(SpringbootApplication.class, args);
+        SpringApplication.run(SpringbootApplication.class, args);
 
         // String[] arg = new String[1];
         // arg[0] = "--server.port=8081";
@@ -22,20 +17,20 @@ public class SpringbootApplication {
         // // 断开读取外部临时配置对应的入口，也就是去掉读取外部参数的形参
         // SpringApplication.run(SpringbootApplication.class);
 
-        // 第三方bean属性绑定
-        ConfigurableApplicationContext run = SpringApplication.run(SpringbootApplication.class, args);
-        Source source = run.getBean(Source.class);
-        System.out.println(source);
-        DruidDataSource ds = run.getBean(DruidDataSource.class);
-        System.out.println(ds.getDriverClassName());
+        // // 第三方bean属性绑定
+        // ConfigurableApplicationContext run = SpringApplication.run(SpringbootApplication.class, args);
+        // Source source = run.getBean(Source.class);
+        // System.out.println(source);
+        // DruidDataSource ds = run.getBean(DruidDataSource.class);
+        // System.out.println(ds.getDriverClassName());
 
     }
 
-    @Bean
-    @ConfigurationProperties(prefix = "datasource")
-    public DruidDataSource datasource() {
-        DruidDataSource ds = new DruidDataSource();
-        return ds;
-    }
+    // @Bean
+    // @ConfigurationProperties(prefix = "datasource")
+    // public DruidDataSource datasource() {
+    //     DruidDataSource ds = new DruidDataSource();
+    //     return ds;
+    // }
 
 }
--- src/test/java/com/example/WebTest.java
+++ src/test/java/com/example/WebTest.java
@@ -1,7 +1,13 @@
 package com.example;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 /**
  * WebTest
@@ -10,10 +16,15 @@ import org.springframework.boot.test.context.SpringBootTest;
  * @since 0.0.1
  */
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+// 开启虚拟MVC调用
+@AutoConfigureMockMvc
 public class WebTest {
 
     @Test
-    void test() {
-
+    void test(@Autowired MockMvc mvc) throws Exception {
+        // 创建虚拟请求，当前访问/books
+        MockHttpServletRequestBuilder builder = MockMvcRequestBuilders.get("/books");
+        // 执行请求
+        ResultActions action = mvc.perform(builder);
     }
 }
