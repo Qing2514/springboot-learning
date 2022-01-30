ab2a0c81dadc6501bd46ad270a6180ff8b6f7038
Qing2514 <2514632453@qq.com>
2022-01-30 16:00:03 +0800

    feat: 添加bean属性校验

--- src/main/java/com/example/domain/Source.java
+++ src/main/java/com/example/domain/Source.java
@@ -7,7 +7,9 @@ import org.springframework.boot.convert.DurationUnit;
 import org.springframework.stereotype.Component;
 import org.springframework.util.unit.DataSize;
 import org.springframework.util.unit.DataUnit;
+import org.springframework.validation.annotation.Validated;
 
+import javax.validation.constraints.Max;
 import java.time.Duration;
 import java.time.temporal.ChronoUnit;
 
@@ -21,11 +23,13 @@ import java.time.temporal.ChronoUnit;
 @Component
 // "" 中不能有大写
 @ConfigurationProperties("source")
+// 开启对当前bean的属性注入校验
+@Validated
 public class Source {
     private String driver;
     private String url;
-    private String username;
-    private String password;
+    @Max(value = 8888, message = "最大值不能超过8888")
+    private int port;
     @DurationUnit(ChronoUnit.DAYS)
     private Duration timeout;
     @DataSizeUnit(DataUnit.KILOBYTES)
