864bf6a9cdb547a73c5b2b65e286ed8c2d47ad8f
Qing2514 <2514632453@qq.com>
2022-01-30 16:53:09 +0800

    feat: 测试类中启动web环境

---
+++ src/test/java/com/example/WebTest.java
@@ -0,0 +1,19 @@
+package com.example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+/**
+ * WebTest
+ *
+ * @author Qing2514
+ * @since 0.0.1
+ */
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class WebTest {
+
+    @Test
+    void test() {
+
+    }
+}
